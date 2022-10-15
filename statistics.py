f23c0f15176a733be3f7a0a2cf60b543f3974548
Kamaraj <kamarajkamarajanis@gmail.com>
2022-10-15 21:31:07 +0530

    update test_raise_alerts_when_max_above_threshold testcase and Releated Class definition

--- statistics.py
+++ statistics.py
@@ -3,3 +3,22 @@ def calculateStats(numbers):
   if len(numbers)==0:
     return {"avg":math.nan,"max":math.nan,"min":math.nan,}
   return {"avg":sum(numbers)/len(numbers),"max":max(numbers),"min":min(numbers)}
+
+
+class EmailAlert:
+  emailSent=False # By Default email sent is False , Change to True if checkAndAlert function identifies a maxThreshold
+
+class LEDAlert:
+  ledGlows=False # By Default led Glows is False , Change to True if checkAndAlert function identifies a maxThreshold
+
+
+class StatsAlerter:
+  def __init__(self,maxThreshold,alertfunction):
+    self.maxThershold=maxThreshold
+    self.alertfunction=alertfunction
+
+  def checkAndAlert(self,numbers):
+    self.numbers=numbers
+    if max(self.numbers)>self.maxThershold:
+      self.alertfunction[0]=True
+      self.alertfunction[1]=True
